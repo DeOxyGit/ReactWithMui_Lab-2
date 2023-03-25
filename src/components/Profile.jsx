653b89c59d34ec32c41a99ec7b739870de267e4f
Ratchanon-Promsombut <ratchanon.promsombut@gmail.com>
2023-03-26 00:43:43 +0700

    commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+function Profile(){
+    return (
+        <div className="layout">
+            <img src="src\assets\S__20611081.jpg" alt="profile" />
+            <h1 className="name">Ratchanon Promsombut </h1>
+            <hr></hr>
+        </div>
+    )
+}
+export default Profile;
